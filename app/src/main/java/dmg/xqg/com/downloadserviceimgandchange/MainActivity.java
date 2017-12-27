c596557bb65868636c597ed5f94426bf902f3681
Chris <395932265@qq.com>
2017-12-27 14:31:30 +0800

    cd

--- app/src/main/java/dmg/xqg/com/downloadserviceimgandchange/MainActivity.java
+++ app/src/main/java/dmg/xqg/com/downloadserviceimgandchange/MainActivity.java
@@ -1,14 +1,10 @@
 package dmg.xqg.com.downloadserviceimgandchange;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -38,25 +34,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "click", Toast.LENGTH_LONG).show();
-                getImgAndSave();
+                handkeImg();
             }
         });
 
         // 第一次进来先显示上一次下载好的，再下载，
-        // 有变化才去下载？给个标志位？
-        Bitmap bitmapDownloaded = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() + "/com.pigamewallet/Record/" + "niamdeaaaa" + ".jpg");
-        Log.e("chris","bitmapDownloaded=" + bitmapDownloaded);
+        // 有变化才去下载？后台给个标志位？
+        Bitmap bitmapDownloaded = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() + "/com.pigamewallet/Record/" + "niamdeaaaa" + ".jpg");// 这里多个斜杠的
         if (bitmapDownloaded != null){
             imageView.setImageBitmap(bitmapDownloaded);
         };
 
     }
 
-    private void getImgAndSave() {
+    private void handkeImg() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                getImageInputStream("http://pic.4j4j.cn/upload/pic/20130617/55695c3c95.jpg");
+                getImgAndSave("http://pic.4j4j.cn/upload/pic/20130617/55695c3c95.jpg");
             }
         }).start();
     }
@@ -67,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
      * @param imageurl 图片网络地址
      * @return Bitmap 返回位图
      */
-    public void getImageInputStream(String imageurl) {
+    public void getImgAndSave(String imageurl) {
         URL url;
         HttpURLConnection connection = null;
         Bitmap bitmap = null;
@@ -82,24 +77,24 @@ public class MainActivity extends AppCompatActivity {
             inputStream.close();
 
             // 保存
-            saveQrCodePicture(bitmap);
+            savePicture(bitmap,"name");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void saveQrCodePicture(Bitmap bitmap) {
+    private void savePicture(Bitmap bitmap,String name) {
 
         File folder = new File(Environment.getExternalStorageDirectory()  + "/com.pigamewallet/Record");
-        if (!folder.exists()) {
+        if (!folder.exists()) {// 文件夹
             folder.mkdirs();
         }
-        final File qrImage = new File(Environment.getExternalStorageDirectory() + "/com.pigamewallet/Record", "niamdeaaaa" + ".jpg");
-        if (qrImage.exists()) {
+        final File qrImage = new File(folder, name + ".jpg");
+        if (qrImage.exists()) {// 每次覆盖掉之前的
             qrImage.delete();
         }
         try {
-            qrImage.createNewFile();
+            qrImage.createNewFile();// 创建新的
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -113,7 +108,6 @@ public class MainActivity extends AppCompatActivity {
         try {
             fOut.flush();
             fOut.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
